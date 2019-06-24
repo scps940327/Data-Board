f430039bf076810cd8ab5f71a9d3f7268fac1dc7
Lauren <scps940327@yahoo.com.tw>
2019-06-24 20:49:58 +0800

    【更新】React 專案初始化
    - React 專案初始化

---
+++ src/js/Body.js
@@ -0,0 +1,12 @@
+import React,{ PropTypes ,useState, useEffect } from 'react';
+import ReactDOM from "react-dom";
+
+function Body(){
+   return(
+      <div></div>
+   )
+}
+
+
+export default Body;
+ReactDOM.render(<Body/>, document.getElementById('root'));
